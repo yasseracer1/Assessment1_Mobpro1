649132e25b3da66b5e32f155b04ee4197340c7b6
Yasser Abdulah Ramadhan <yasseracer1@gmail.com>
2024-04-01 23:18:59 +0700

    Membuat package screen sebagai tempat menaruh halaman tampilan aplikasi yang berbeda

--- app/src/main/java/org/d3if3130/assessment1_mobpro1/MainActivity.kt
+++ app/src/main/java/org/d3if3130/assessment1_mobpro1/MainActivity.kt
@@ -1,22 +1,13 @@
 package org.d3if3130.assessment1_mobpro1
 
-import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
+import org.d3if3130.assessment1_mobpro1.ui.screen.MainScreen
 import org.d3if3130.assessment1_mobpro1.ui.theme.Assessment1_Mobpro1Theme
 
 class MainActivity : ComponentActivity() {
@@ -29,49 +20,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainScreen()
                 }
             }
         }
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun MainScreen(content: @Composable (Modifier) -> Unit) {
-    Scaffold (
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text(text = stringResource(id = R.string.app_name))
-                },
-                colors = TopAppBarDefaults.mediumTopAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary
-                )
-            )
-        }
-    ) {
-        padding ->
-        content(Modifier.padding(padding))
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    MainScreen { modifier ->
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
-        )
-    }
-}
-
-@Preview(showBackground = true)
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES,showBackground = true)
-@Composable
-fun ScreenPreview() {
-    Assessment1_Mobpro1Theme {
-        Greeting("Android")
     }
 }
