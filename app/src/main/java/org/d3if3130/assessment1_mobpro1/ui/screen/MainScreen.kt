369702f34ab5de96bfb9dab67f5464ffaa63de08
Yasser Abdulah Ramadhan <yasseracer1@gmail.com>
2024-04-02 00:22:19 +0700

    Membuat opsi susu menggunakan radio button

--- app/src/main/java/org/d3if3130/assessment1_mobpro1/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if3130/assessment1_mobpro1/ui/screen/MainScreen.kt
@@ -1,15 +1,20 @@
 package org.d3if3130.assessment1_mobpro1.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -18,9 +23,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
@@ -53,6 +62,14 @@ fun MainScreen() {
 @Composable
 fun ScreenContent(modifier: Modifier) {
     var note by remember { mutableStateOf("") }
+
+    val radioOptions = listOf(
+        stringResource(id = R.string.coklat),
+        stringResource(id = R.string.putih)
+    )
+
+    var susu by rememberSaveable { mutableStateOf(radioOptions[0]) }
+
     Column (
         modifier = modifier
             .fillMaxSize()
@@ -64,6 +81,26 @@ fun ScreenContent(modifier: Modifier) {
             style = MaterialTheme.typography.bodyLarge,
             modifier = Modifier.fillMaxWidth()
         )
+        Row (
+            modifier = Modifier
+                .padding(top = 6.dp)
+                .border(1.dp, Color.Gray, RoundedCornerShape(4.dp))
+        ) {
+            radioOptions.forEach {text ->
+                RasaOption(
+                    label = text,
+                    isSelected = susu == text,
+                    modifier = Modifier
+                        .selectable(
+                            selected = susu == text,
+                            onClick = { susu = text },
+                            role = Role.RadioButton
+                        )
+                        .weight(1f)
+                        .padding(16.dp)
+                )
+            }
+        }
         OutlinedTextField(
             value = note,
             onValueChange = { note = it },
@@ -74,6 +111,22 @@ fun ScreenContent(modifier: Modifier) {
             ),
             modifier = Modifier.fillMaxWidth()
         )
+    }
+}
+
+@Composable
+fun RasaOption(label: String, isSelected: Boolean, modifier: Modifier) {
+    Row (
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        RadioButton(selected = isSelected, onClick = null)
+        Text(
+            text = label,
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.padding(start = 8.dp)
+        )
+
     }
 }
 
