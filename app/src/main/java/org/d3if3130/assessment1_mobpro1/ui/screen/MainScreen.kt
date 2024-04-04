16d28c5a720b917621d4350f41a0777737360791
Yasser Abdulah Ramadhan <yasseracer1@gmail.com>
2024-04-05 00:13:17 +0700

    Membuat fungsional Pemesanan dan bagikan

--- app/src/main/java/org/d3if3130/assessment1_mobpro1/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if3130/assessment1_mobpro1/ui/screen/MainScreen.kt
@@ -1,18 +1,27 @@
 package org.d3if3130.assessment1_mobpro1.ui.screen
 
+import android.content.Context
+import android.content.Intent
 import android.content.res.Configuration
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.Checkbox
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
@@ -29,6 +38,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.input.ImeAction
@@ -64,6 +74,9 @@ fun MainScreen() {
 fun ScreenContent(modifier: Modifier) {
     var note by remember { mutableStateOf("") }
 
+    var nama by remember { mutableStateOf("") }
+    var namaError by rememberSaveable { mutableStateOf(false) }
+
     val susuOptions = listOf(
         stringResource(id = R.string.coklat),
         stringResource(id = R.string.putih)
@@ -86,14 +99,38 @@ fun ScreenContent(modifier: Modifier) {
     var rasa by rememberSaveable { mutableStateOf(rasaOptions[0]) }
     var topping by rememberSaveable { mutableStateOf(checkBox[0]) }
 
+    var pesan by rememberSaveable { mutableStateOf("") }
+    val context = LocalContext.current
+
     Column (
         modifier = modifier
             .fillMaxSize()
+            .verticalScroll(rememberScrollState())
             .padding(16.dp),
-        verticalArrangement = Arrangement.spacedBy(12.dp)
+        verticalArrangement = Arrangement.spacedBy(12.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ){
         Text(
             text = stringResource(id = R.string.intro),
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = nama,
+            onValueChange = { nama = it },
+            label = { Text(text = stringResource(id = R.string.nama)) },
+            isError = namaError,
+            trailingIcon = { IconPicker(isError = namaError, unit = "")},
+            supportingText = { ErrorHint(isError = namaError)},
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Done
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+        Text(
+            text = stringResource(id = R.string.susu),
             style = MaterialTheme.typography.bodyLarge,
             modifier = Modifier.fillMaxWidth()
         )
@@ -126,6 +163,7 @@ fun ScreenContent(modifier: Modifier) {
             modifier = Modifier
                 .padding(top = 6.dp)
                 .border(1.dp, Color.Gray, RoundedCornerShape(4.dp))
+                .fillMaxWidth()
         ) {
             rasaOptions.forEach {text ->
                 RasaOption(
@@ -150,6 +188,7 @@ fun ScreenContent(modifier: Modifier) {
             modifier = Modifier
                 .padding(top = 6.dp)
                 .border(1.dp, Color.Gray, RoundedCornerShape(4.dp))
+                .fillMaxWidth()
         ) {
             checkBox.forEach {text ->
                 ToppingOption(
@@ -165,7 +204,6 @@ fun ScreenContent(modifier: Modifier) {
                 )
             }
         }
-
         OutlinedTextField(
             value = note,
             onValueChange = { note = it },
@@ -176,6 +214,43 @@ fun ScreenContent(modifier: Modifier) {
             ),
             modifier = Modifier.fillMaxWidth()
         )
+        Button(
+            onClick = {
+                namaError = (nama == "")
+                if (namaError) return@Button
+
+                pesan = context.getString(R.string.pesanan, nama, susu, rasa, topping, note)
+            },
+
+            modifier = Modifier.padding(top = 8.dp),
+            contentPadding = PaddingValues(horizontal = 32.dp, vertical = 16.dp),
+        ) {
+            Text(text = stringResource(id = R.string.pesan))
+        }
+
+        if (pesan.isNotEmpty()) {
+            Divider(
+                modifier = Modifier.padding(vertical = 8.dp),
+                thickness = 1.dp
+            )
+            Text(
+                text = pesan,
+                style = MaterialTheme.typography.titleLarge
+            )
+            Button(
+                onClick = {
+                    shareData(
+                        context = context,
+                        message = context.getString(R.string.bagikan_template,
+                            nama, susu, rasa, topping, note)
+                    )
+                },
+                modifier = Modifier.padding(top = 8.dp),
+                contentPadding = PaddingValues(horizontal = 32.dp, vertical = 16.dp)
+            ) {
+                Text(text = stringResource(id = R.string.bagikan))
+            }
+        }
     }
 }
 
@@ -207,7 +282,6 @@ fun RasaOption(label: String, isSelected: Boolean, modifier: Modifier) {
             style = MaterialTheme.typography.bodyLarge,
             modifier = Modifier.padding(start = 8.dp)
         )
-
     }
 }
 
@@ -217,13 +291,39 @@ fun ToppingOption(label: String, isSelected: Boolean, modifier: Modifier) {
         modifier = modifier,
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Checkbox(checked = isSelected, onCheckedChange = null)
+        RadioButton(selected = isSelected, onClick = null)
         Text(
             text = label,
             style = MaterialTheme.typography.bodyLarge,
             modifier = Modifier.padding(start = 8.dp)
         )
 
+    }
+}
+
+@Composable
+fun IconPicker(isError: Boolean, unit: String) {
+    if (isError) {
+        Icon(imageVector = Icons.Filled.Warning, contentDescription = null)
+    } else {
+        Text(text = unit)
+    }
+}
+
+@Composable
+fun ErrorHint(isError: Boolean) {
+    if (isError) {
+        Text(text = stringResource(id = R.string.input_invalid))
+    }
+}
+
+private fun shareData(context : Context, message: String) {
+    val shareIntent = Intent(Intent.ACTION_SEND).apply {
+        type = "text/plain"
+        putExtra(Intent.EXTRA_TEXT, message)
+    }
+    if (shareIntent.resolveActivity(context.packageManager) != null) {
+        context.startActivity(shareIntent)
     }
 }
 
